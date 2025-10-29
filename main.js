101893c02372c369846744ca8096eaeac4b4adc3
YukiAoi <xxz530990200@gmail.com>
2025-10-29 17:41:59 +0800

    预加载脚本

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
-import { app, BrowserWindow } from 'electron'
+import { app, ipcMain, BrowserWindow } from 'electron'
+import path from "node:path";
+import { fileURLToPath } from 'url'
+
+// 手动定义 __dirname
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, './preload/index.js')
+    }
   })
 
   // 下面的url为自己启动vite项目的url。
   win.loadURL('http://localhost:5173/')
   // win.loadFile('index.html')
 }
+
+ipcMain.handle('sent-event', (event, params) => {
+  console.log(params)
+  return '1111'
+})
 
 app.whenReady().then(() => {
   createWindow()
---
+++ preload/index.js
@@ -0,0 +1,9 @@
+import { contextBridge, ipcRenderer } from 'electron'
+const handleSend = async (vue_params) => {
+  let fallback = await ipcRenderer.invoke('sent-event', vue_params)
+  return fallback
+}
+contextBridge.exposeInMainWorld('myApi', {
+  handleSend: handleSend
+  // 可以暴露函数和变量
+})
